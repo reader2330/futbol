0c29a8dbc5e56b78bc61c6a69dba9d5952ba5b3e
joaquinhmtz <joaquin_downhill@hotmail.com>
2017-07-10 17:07:32 -0500

    noticia

---
+++ Futbol/application/views/futbol/articulo2.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/articulo12.css">
+
+<div class="container">
+	
+
+	<br>
+	<br>
+	<br>
+	<h1 align="center">Nuevo Integrante del Real Madrid, de tan solo 19 años.</h1>
+	<br>
+	<br>
+	<br>
+
+	<div class="imagen">
+		<img src="<?= base_url() ?>img/theo-hernandez2.jpg" align="center" alt="Theo Hernández">
+	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="contenido">
+		<div class="datos">
+
+			<div class="autor">
+				<p align="center">Hecho : Joaquín Hernández
+					@downhilljoaquin
+				</p>
+
+			</div>
+
+		</div>
+		<div class="texto">
+
+
+
+			<p>Theo Hernández el francés ex-lateral del Atlético de Madrid, ya es parte del equipo del Real Madrid. <br>
+			Fue presentado el día de hoy en el Santiago Bernabéu firmando un contrato de seis temporadas hasta el año 2023 con el equipo.
+			</p>
+
+			<br>
+			<br>		
+			<img src="<?= base_url() ?>img/theo-hernandez2.jpg" align="center" alt="Theo Hernández">
+			<br>
+			<br>
+			<p>
+				Theo Hernández no sólo va a heredar el puesto de sus antecesores, si no también ha eligido el dorsal con el número 15 de Fabio Coentrao. El nuevo lateral del equipo meregue dijo unas palabras de agradecimiento: <br>
+				Theo: “Estoy muy contento de estar en el mejor club del mundo”
+				“Muchas gracias al presidente, a la afición y al equipo por el recibimiento que me han dado. Estoy muy contento de estar en el mejor club del mundo. He venido para aprender de los mejores y ganar muchos títulos juntos. Muchas gracias a todos y hala Madrid”.
+			</p>
+
+			<img src="<?= base_url() ?>img/theo-hernandez2.jpg" align="center" alt="Theo Hernández">
+
+			<p>
+				
+				Para concluir, el nuevo lateral Theo Hernández piso por primera vez el estadio de los meregues, el Santiago Bernabéu, se vistió con el uniforme oficial del equipo mostrando el dorsal con el número 15, concluyendo, Theo Hernández finalizó regalando balones a los aficionados quién acudieron a la presentación desde las gradas.
+			</p>
+
+			<br>
+			<br>
+
+			
+
+
+
+		</div>
+	</div>
+
+
+
+
+
+
+
+</div>	
