a2f05a77d8a887e5679665bc7d2725dd74902874
reader2330 <fernando@MacBook-Pro-de-Fernando.local>
2017-07-14 21:10:41 -0500

    responsive

--- Futbol/application/views/futbol/Inicio.php
+++ Futbol/application/views/futbol/Inicio.php
@@ -6,7 +6,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <head>
 
-<link rel="stylesheet" type="text/css" href="<? base_url() ?>/assets/css/botones5.css">
+	<link rel="stylesheet" type="text/css" href="<? base_url() ?>assets/css/botones5.css">
+	<link rel="stylesheet" type="text/css" href="<? base_url() ?>assets/css/estilos6.css">
+	<link rel="stylesheet" type="text/css" href="<? base_url() ?>assets/css/inicior6.css">
+
+
 
 
 </head>
@@ -24,299 +28,319 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 			<div class="carousel-inner" role="listbox">
 				<div class="carousel-item active">
-					<a href="<?= base_url(); ?>"><img class="d-block img-fluid" src="<?= base_url();?>img/Osorio/Osorio2.jpg" alt="Futbol" >
-					
-				</div>
-				<div class="carousel-item">
-					<img class="d-block img-fluid" src="<?= base_url();?>img/Pulido/Pulido.jpg" alt="Second slide">
-					
-				</div>
-				<div class="carousel-item">
-					<img class="d-block img-fluid" src="<?= base_url();?>img/Osorio/osorio21.jpg" alt="Third slide">
-					<div class="carousel-caption d-none d-md-block">
+					<img class="d-block img-fluid" src="<?= base_url();?>img/Osorio/Osorio2.jpg" alt="First slide" >
+
+					</div>
+					<div class="carousel-item">
+						<img class="d-block img-fluid" src="<?= base_url();?>img/Pulido/Pulido.jpg" alt="Second slide">
+
+					</div>
+					<div class="carousel-item">
+						<img class="d-block img-fluid" src="<?= base_url();?>img/Osorio/osorio21.jpg" alt="Third slide">
 						
 					</div>
 				</div>
+				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
 
-		<br>
-		<br>
-		<br>
+			<br>
+			<br>
+			<br>
 
 
-		
-
-		<div class="resultados">
 
 
-			<div class="texto">
-				<h2 align="center">Copa ORO 2017</h2>
-				<br>
-				<br>
-				<center class="centrar">
-					<a href="<? base_url(); ?>Categorias/CopaOro" class="btn btn-primary" id="inicio" style="margin-left:15px">Leer más</a>
-				</center>
-			</div>
-			<div class="resultado">
+			<div class="resultados">
 
 
-				<div class="card">
+				<div class="texto">
+					<h2 align="center">Copa ORO 2017</h2>
+					<br>
+					<br>
+					<center class="centrar">
+						<a href="<? base_url(); ?>Categorias/CopaOro" class="btn btn-primary" id="inicio" style="margin-left:15px">Leer más</a>
+					</center>
+				</div>
+				<div class="resultado">
 
-					<div class="card-block">
-						<div class="izq">
+
+					<div class="card">
+
+						<div class="card-block">
+							<div class="izq">
+								<br>
+								<p align="center"> Guayana </p>
+								<img src="<?= base_url();?>img/paises/guayana.png" width=100 alt="guayana">
+								<br>
+								<br>
+								<p align="center"> 2</p>
+							</div>
+							<strong>VS</strong>
+							<div class="der"> 
+								<br>
+								<p align="center">Canada</p>
+								<img src="<?= base_url(); ?>img/paises/canada.png" width=120 alt="canada"> 
+								<br>
+								<br>
+								<p align="center">4</p>
+							</div>  
+
 							<br>
-							<p align="center"> Guayana </p>
-							<img src="<?= base_url();?>img/paises/guayana.png" width=100 alt="guayana">
 							<br>
-							<br>
-							<p align="center"> 2</p>
+
 						</div>
-						<strong>VS</strong>
-						<div class="der"> 
-							<br>
-							<p align="center">Canada</p>
-							<img src="<?= base_url(); ?>img/paises/canada.png" width=120 alt="canada"> 
-							<br>
-							<br>
-							<p align="center">4</p>
-						</div>  
+					</div>
 
-						<br>
-						<br>
+				</div>
+				<div class="resultado">
+					<div class="card">
 
+						<div class="card-block">
+							<div class="izq">
+								<br>
+								<p align="center"> Honduras </p>
+								<img src="<?= base_url();?>img/paises/honduras.png" width=100 alt="honduras">
+								<br>
+								<br>
+								<p align="center"> 0</p>
+							</div>
+							<strong>VS</strong>
+							<div class="der"> 
+								<br>
+								<p align="center">Costa Rica</p>
+								<img src="<?= base_url(); ?>img/paises/costarica.png" width=100 alt="costarica"> 
+								<br>
+								<br>
+								<p align="center"> 1 </p>
+							</div>  
+
+							<br>
+							<br>
+
+						</div>
 					</div>
 				</div>
-				
-			</div>
-			<div class="resultado">
-				<div class="card">
+				<div class="resultado">
+					<div class="card">
 
-					<div class="card-block">
-						<div class="izq">
+						<div class="card-block">
+							<div class="izq">
+								<br>
+								<p align="center"> USA </p>
+								<img src="<?= base_url();?>img/paises/usa.png" width=100 alt="USA">
+								<br>
+								<br>
+								<p align="center"> 1</p>
+							</div>
+							<strong>VS</strong>
+							<div class="der"> 
+								<br>
+								<p align="center">Panama</p>
+								<img src="<?= base_url(); ?>img/paises/panama.png" width=100 alt="Panama"> 
+								<br>
+								<br>
+								<p align="center"> 1 </p>
+							</div>  
+
 							<br>
-							<p align="center"> Honduras </p>
-							<img src="<?= base_url();?>img/paises/honduras.png" width=100 alt="honduras">
 							<br>
-							<br>
-							<p align="center"> 0</p>
+
 						</div>
-						<strong>VS</strong>
-						<div class="der"> 
-							<br>
-							<p align="center">Costa Rica</p>
-							<img src="<?= base_url(); ?>img/paises/costarica.png" width=100 alt="costarica"> 
-							<br>
-							<br>
-							<p align="center"> 1 </p>
-						</div>  
-
-						<br>
-						<br>
-
 					</div>
 				</div>
-			</div>
-			<div class="resultado">
-				<div class="card">
+				<div class="resultado">
+					<div class="card">
 
-					<div class="card-block">
-						<div class="izq">
+						<div class="card-block">
+							<div class="izq">
+
+								<p align="center">Martinica</p>
+								<img src="<?= base_url();?>img/paises/martinica.png" width=80 alt="Martinica">
+
+
+								<p align="center"> 2</p>
+							</div>
+							<strong>VS</strong>
+							<div class="der"> 
+
+								<p align="center">Nicaragua</p>
+
+
+								<img src="<?= base_url(); ?>img/paises/nicaragua.png" width=100 alt="nicaragua"> 
+								<br>
+								<br>
+								<p align="center"> 0 </p>
+							</div>  
+
 							<br>
-							<p align="center"> USA </p>
-							<img src="<?= base_url();?>img/paises/usa.png" width=100 alt="USA">
 							<br>
-							<br>
-							<p align="center"> 1</p>
+
 						</div>
-						<strong>VS</strong>
-						<div class="der"> 
-							<br>
-							<p align="center">Panama</p>
-							<img src="<?= base_url(); ?>img/paises/panama.png" width=100 alt="Panama"> 
-							<br>
-							<br>
-							<p align="center"> 1 </p>
-						</div>  
-
-						<br>
-						<br>
-
 					</div>
 				</div>
-			</div>
-			<div class="resultado">
-			<div class="card">
+				<div class="resultado">
+					<div class="card">
 
-					<div class="card-block">
-						<div class="izq">
+						<div class="card-block">
+							<div class="izq">
+								<br>
+								<p align="center">Curazao</p>
+								<img src="<?= base_url();?>img/paises/curazao.png" width=80 alt="Curazao">
+								<br>
+								<br>
+
+								<p align="center">0</p>
+							</div>
+							<strong>VS</strong>
+							<div class="der"> 
+								<br>
+								<p align="center">Jamaica</p>
+								<img src="<?= base_url(); ?>img/paises/jamaica.png" width=90 alt="Jamaica"> 
+								<br>
+								<br>
+								<p align="center">2</p>
+							</div>  
+
+							<br>
+							<br>
+
+						</div>
+					</div>
+				</div>
+				<div class="resultado">
+					<div class="card">
+
+						<div class="card-block">
+							<div class="izq">
+
+								<p align="center"> México</p>
+								<img src="<?= base_url();?>img/paises/mexico.jpg" width=100 alt="Mexico">
+								<br>
+								<br>
+								<p align="center">3</p>
+							</div>
+							<strong>VS</strong>
+							<div class="der"> 
+
+								<p align="center">El Salvador</p>
+								<img src="<?= base_url(); ?>img/paises/salvador.png" width=100 alt="Salvador"> 
+								<br>
+								<br>
+
+								<p align="center">1</p>
+							</div>  
+
+							<br>
+							<br>
+
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+
+			<br>
+			<br>
+			<br>
+
+			<br>
+			<h1 align="center">Ultimos Articulos</h1>
+			<br>
+			<br>
+			<br>
+
+
+
+
+
+			<div class="noticias">
+
+				<div class="noticia">
+					<div class="card" id="card">
+						<div class="card-block">
+							<a href="<? base_url(); ?>Articulo/tecatito_rompe" style="text-decoration: none;"><h3>Rompe el silencio el tecatito Corona</h3></a>
+						</div>
+						<br>
+						<a href="<? base_url(); ?>Articulo/tecatito_rompe" style="text-decoration: none;"><img src="<? base_url(); ?>img/Tecatito/tecatito.jpg" alt="Tecatito" width=100% ></a>
+						<br>
+						<div class="card-block">
+							<p class="card-text">
+								Tecatito: "Estoy listo, no es nada con México"</p>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="noticia" >
+						<div class="card" id="card">
+							<div class="card-block">
+							<a href="<? base_url(); ?>Articulo/juventus_cancela" style="text-decoration: none;"><h3>Juventus cancela partido frente a Monterrey y Tigres.</h3></a>
+							</div>
 							
-							<p align="center">Martinica</p>
-							<img src="<?= base_url();?>img/paises/martinica.png" width=80 alt="Martinica">
-							
-				
-							<p align="center"> 2</p>
-						</div>
-						<strong>VS</strong>
-						<div class="der"> 
-
-							<p align="center">Nicaragua</p>
+							<a href="<? base_url(); ?>Articulo/juventus_cancela" style="text-decoration: none;"><img src="img/Juventus/juve.jpg" alt="Jueventus" width="100%"></a>
+							<br>
 							
 						
-							<img src="<?= base_url(); ?>img/paises/nicaragua.png" width=100 alt="nicaragua"> 
-							<br>
-							<br>
-							<p align="center"> 0 </p>
-						</div>  
-
-						<br>
-						<br>
-
-					</div>
-				</div>
-			</div>
-			<div class="resultado">
-				<div class="card">
-
-					<div class="card-block">
-						<div class="izq">
-							<br>
-							<p align="center">Curazao</p>
-							<img src="<?= base_url();?>img/paises/curazao.png" width=80 alt="Curazao">
-							<br>
-							<br>
-							
-							<p align="center">0</p>
+							<div class="card-block">
+								<p class="card-text"> El equipo Campeón de la Serie A anunció la cancelación de los compromisos de pretemporada en México </p>
+							</div>
 						</div>
-						<strong>VS</strong>
-						<div class="der"> 
-							<br>
-							<p align="center">Jamaica</p>
-							<img src="<?= base_url(); ?>img/paises/jamaica.png" width=90 alt="Jamaica"> 
-							<br>
-							<br>
-							<p align="center">2</p>
-						</div>  
-
-						<br>
-						<br>
-
 					</div>
-				</div>
-			</div>
-			<div class="resultado">
-				<div class="card">
 
-					<div class="card-block">
-						<div class="izq">
-							 
-							<p align="center"> México</p>
-							<img src="<?= base_url();?>img/paises/mexico.jpg" width=100 alt="Mexico">
+					<div class="noticia">
+						<div class="card" id="card">
+						<br>
+							<div class="card-block">
+								<br>
+								<a href="<? base_url(); ?>Articulo/chivas_playera" style="text-decoration: none;"><h3>Chivas presume la 12 en flamante jersey para Apertura 2017</h3></a>
+							</div>
 							<br>
-							<br>
-							<p align="center">3</p>
+							<a href="<? base_url(); ?>Articulo/chivas_playera" style="text-decoration: none;"><img src="img/chivas_playera/chivas1.png" alt="chivas" width="100%"></a>
+							<div class="card-block">
+								<p class="card-text">El Guadalajara presentó el uniforme de local que portará para la próxima temporada</p>
+							</div>
 						</div>
-						<strong>VS</strong>
-						<div class="der"> 
-							
-							<p align="center">El Salvador</p>
-							<img src="<?= base_url(); ?>img/paises/salvador.png" width=100 alt="Salvador"> 
-							<br>
-							<br>
-						
-							<p align="center">1</p>
-						</div>  
+					</div>
 
-						<br>
-						<br>
+			</div>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+				<h2 align="center">Nuestras publicaciones en las Redes Sociales</h2>
+
+				<div class="time">
+
+					<div class="fb">
+
+						<div class="fb-page" data-href="https://www.facebook.com/Patada-Inicial-480148992324550/?ref=aymt_homepage_panel" data-tabs="timeline" data-width="500" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Patada-Inicial-480148992324550/?ref=aymt_homepage_panel" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Patada-Inicial-480148992324550/?ref=aymt_homepage_panel">Patada Inicial</a></blockquote></div>
+
 
 					</div>
+					<div class="tw">
+						<a class="twitter-timeline" href="https://twitter.com/patadainicialof">Tweets by patadainicialof</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+					</div>
+
+
+
 				</div>
-			</div>
-
-
-		</div>
-
-
-
-		<h1 align="center">Ultimos Articulos</h1>
-		<br>
-		<br>
-
-
-
-
-
-		<div class="noticias">
-
-			<div class="noticia">
-				<div class="card">
-					<div class="card-block">
-						<p class="card-text">Las mejores noticias del momento sobre el futbol</p>
-					</div>
-					<img src="/images/pathToYourImage.png" alt="Card image">
-					<div class="card-block">
-						<p class="card-text">Nos dedicamos a mantenerte informado sobre las ultimas noticias sobre este asombro deporte</p>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="noticia">
-				<div class="card">
-					<div class="card-block">
-						<p class="card-text">Los Resultados al momento</p>
-					</div>
-					<img src="/images/pathToYourImage.png" alt="Card image">
-					<div class="card-block">
-						<p class="card-text">Todos los resultados a tu alcance y de forma rapida</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="noticia">
-				<div class="card">
-					<div class="card-block">
-						<p class="card-text">Intereacción con todos</p>
-					</div>
-					<img src="/images/pathToYourImage.png" alt="Card image">
-					<div class="card-block">
-						<p class="card-text">Aqui encontraras una forma de expresar tus opiniones y compratir tus mejores ideas</p>
-					</div>
-				</div>
-			</div>
-
-		</div>
-
-
-		<h2 align="center">Nuestras publicaciones en las Redes Sociales</h2>
-
-		<div class="time">
-
-			<div class="fb">
-
-				<div class="fb-page" data-href="https://www.facebook.com/Patada-Inicial-480148992324550/?ref=aymt_homepage_panel" data-tabs="timeline" data-width="500" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Patada-Inicial-480148992324550/?ref=aymt_homepage_panel" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Patada-Inicial-480148992324550/?ref=aymt_homepage_panel">Patada Inicial</a></blockquote></div>
-
-
-			</div>
-			<div class="tw">
-				<a class="twitter-timeline" href="https://twitter.com/patadainicialof">Tweets by patadainicialof</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 			</div>
 
 
 
-		</div>
-
-	</div>
 
 
 
@@ -328,17 +352,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-
-
-	
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
 
 
 
