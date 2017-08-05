d6ee2fc1a821b6c92cdee7e6b7c0362a6eabb1f8
Raul <rauleto_7@hotmail.com>
2017-08-05 16:44:10 -0500

    Dragonball

---
+++ Futbol/application/views/futbol/DragonBall.php
@@ -0,0 +1,109 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/articulo12.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/articulor6.css">
+<div class="container">
+	
+
+	<br>
+	<br>
+	<br>
+	<h1 align="center">Goku y sus amigos estan de regreso, Dragon Ball super se estrena en Latinoamerica.</h1>
+	<br>
+	<br>
+	<br>
+
+	<div class="imagen">
+		<img src="<?= base_url() ?>img/DragonBall/dragonball12.jpg" align="center" alt="Goku Dragon Ball Super">
+	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="contenido">
+		<div class="datos">
+<br>
+<br>
+			<div class="autor">
+				<p align="center">Hecho : Raúl Escamilla	
+					<br>@rulo_escamilla
+				</p>
+
+			</div>
+
+		</div>
+		<div class="texto">
+
+
+	<br>
+	<br>
+			<p> Dragon Ball Super se estrenará este sábado 5 de agosto en América Latina con las voces originales de su versión en español que los fanáticos conocieron en Dragon Ball Z.
+			<br>
+			Mario Castañeda y René García vuelven a la pantalla chica para dar vida a “Goku” y “Vegeta, respectivamente, además de José Luis Orozco (Bills) y Arturo Castañeda (Wiss).
+			</p>
+
+			
+			
+			<p>
+				La serie de Toei Animation Inc. llega hoy a los hogares latinoamericanos a través de Cartoon Network con un maratón de cuatro episodios para después transmitirse de manera regular de lunes a viernes a partir del 7 de agosto desde las 17:00 hasta las 19:00 horas, luego éstos comenzará a emitirse en su horario habitual que será de lunes a viernes a las 23:00 horas, con repeticiones a las 15:30 horas..				  
+			</p>
+			<br>
+			<br>
+			<img src="<?= base_url() ?>img/DragonBall/DrabonBall1-2.jpg" align="center" alt="Goku Dragon Ball Super">
+			<br>
+			<br>
+			<p>
+
+				A casi 20 años de la última aventura de Dragon Ball Z, Dragon Ball Super retoma el hilo allí donde concluyó la icónica serie y continúa la leyenda con una historia original supervisada por el creador de Dragon Ball, Akira Toriyama. 
+				<br>
+				“Estamos felices y orgullosos de ser el hogar en Latinoamérica de una propiedad que, al igual que Cartoon Network América Latina, ha cultivado fans tan apasionados y genera tanto entusiasmo en la región”, dijo Pablo Zuccarino, Vicepresidente de Gerente de Canal, Cartoon Network, Boomerang y Tooncas América Latina.
+				
+			</p>
+			<br>
+			<br>
+			<img src="<?= base_url() ?>img/DragonBall/DragonBall1-1.jpg" align="center" alt="Goku Dragon Ball Super">
+			<br>
+			<br>
+			<p>
+				“La expectativa por Dragon Ball en Latinoamérica no podría ser más grande, y estamos muy entusiasmados de avanzar con nuestro socio de hace años, Cartoon Network Latinoamérica, para introducir Dragon Ball Super en toda la región” dijo Daniel Castaneda, Director de Licencias de Toei para América Latina. “Latinoamérica ha acogido el animé casi desde sus comienzos y continúa siendo una pieza integral del fenómeno global que es el animé hoy. Dragon Ball Super es tan movilizante, dramático y repleto de acción como siempre y esperamos que nuestros fans latinoamericanos lo reciban con gran entusiasmo”.
+			</p>
+			<br>
+			<br>
+			<br>
+			<h3>Te dejamos con el video:</h3>
+			<br>
+			<br>
+			<video src="<?= base_url() ?>img/DragonBall/DragonBallZ.mp4" width="640" height="360" controls autoplay preload></video>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			
+
+			
+
+
+
+		</div>
+	</div>
+
+
+
+
+
+
+
+</div>	
